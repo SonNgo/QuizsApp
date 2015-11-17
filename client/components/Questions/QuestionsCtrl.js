58cef9802d840e8391f617880580221cf08308ce
nts <ngotungson0602@gmail.com>
2015-11-17 16:47:38 +0700

    add completeWord lesson form

--- client/components/Questions/QuestionsCtrl.js
+++ client/components/Questions/QuestionsCtrl.js
@@ -8,15 +8,36 @@ angular.module('QuizApp')
 	QuestionsService.getByQuiz($stateParams.subject, $stateParams.quiz)
 		.then ( function  (result) {
 			$scope.questions = result.data;
-
+			
 		})
 
 
+	$scope.checkTemplate = function (template, index) {
+		if ($scope.questions[index].category == template) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	
+
+
 	$scope.indexQuestion = 0;
+	$scope.totalPoint = 0;
+	
+
+
 	$scope.currentQuestion = function (index) {
 		if ($scope.indexQuestion != index) return false;
 		else return true;
 	}
+	function updateUserPoint(point) {
+		sharedData.set('point', point);
+	}
+
+
+
 
 	$scope.checked = function (index) {
 		$scope.answer = index;
@@ -26,8 +47,8 @@ angular.module('QuizApp')
 
 		$scope.questions[index].done = true;
 		if (($scope.answer != null) && ($scope.questions[index].answers[$scope.answer].is_correct)) {
-			console.log(index + ' ' + $scope.answer);
 			var point = $scope.questions[index].point;
+			$scope.totalPoint += point;
 			updateUserPoint(point);
 			Notification.success('CORRECT. +' + point + ' point');
 		} else {
@@ -36,13 +57,35 @@ angular.module('QuizApp')
 
 		if(index == $scope.questions.length - 1) {
 			sharedData.set('isFinished', true);
+			$('.ui.small.result.modal')
+        .modal('setting', 'transition', 'fade up')
+  			.modal('show');
 		}
 
 		$scope.indexQuestion = index+1;
 	}
 
-	function updateUserPoint(point) {
-		sharedData.set('point', point);
+	$scope.checkCompleteWordResult = function(index) {
+
+		$scope.questions[index].done = true;
+		console.log(index + ' ' + $scope.questions[index].answerUser + ' ' + $scope.questions[index].answers[0].content);
+		if ($scope.questions[index].answerUser == $scope.questions[index].answers[0].content) {
+			var point = $scope.questions[index].point;
+			$scope.totalPoint += point;
+			updateUserPoint(point);
+			Notification.success('CORRECT. +' + point + ' point');
+		} else {
+			Notification.error('NOT CORRECT');
+		}
+
+		if(index == $scope.questions.length - 1) {
+			sharedData.set('isFinished', true);
+			$('.ui.small.result.modal')
+        .modal('setting', 'transition', 'fade up')
+  			.modal('show');
+		}
+
+		$scope.indexQuestion = index+1;
 	}
 
 }])
